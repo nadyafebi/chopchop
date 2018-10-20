36c1009dbb38362f5f2fad3a19fe9b0b61b4211d
Nadya Febiana Djojosantoso <hello@nadyafebi.com>
2018-10-20 16:17:39 -0400

    Add recipe maker

---
+++ src/pages/admin/admin.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+/**
+ * Generated class for the AdminPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-admin',
+  templateUrl: 'admin.html',
+})
+export class AdminPage {
+  name: string;
+  time: string;
+  ingredients: object[] = [];
+  ingredientName: string;
+  ingredientHowMany: string;
+  steps: string[] = [];
+  step: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public db: AngularFirestore, public loadingCtrl: LoadingController) {
+  }
+
+  addIngredient() {
+    this.ingredients.push({
+      name: this.ingredientName,
+      howMany: this.ingredientHowMany
+    });
+    this.ingredientName = '';
+    this.ingredientHowMany = '';
+  }
+
+  addStep() {
+    this.steps.push(this.step);
+    this.step = '';
+  }
+
+  addRecipe() {
+    const loader = this.loadingCtrl.create({
+      content: 'Adding recipe...'
+    });
+    loader.present();
+
+    this.db.collection('recipes').add({
+      name: this.name,
+      time: this.time,
+      ingredients: this.ingredients,
+      steps: this.steps
+    })
+    .then(ref => {
+      loader.dismiss();
+    });
+  }
+
+}
