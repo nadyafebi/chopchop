ebca6041cd927fa5ae5d13699bd69486893a5ac2
Nadya Febiana Djojosantoso <hello@nadyafebi.com>
2018-10-20 20:15:28 -0400

    Fix bug

--- src/pages/ingredients/ingredients.ts
+++ src/pages/ingredients/ingredients.ts
@@ -35,7 +35,7 @@ export class IngredientsPage {
     var count=this.ingredients.length;
     for(var i=0;i<count;i++)
     {
-      if("this.ingredients[i]=== this.ingredient"){return false;}
+      if(this.ingredients[i]=== this.ingredient){return false;}
     }
     if(this.ingredient == undefined) {
       return false;}
